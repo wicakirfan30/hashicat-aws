8213c32ca922cac35d83290274144a10993bbc4c
Irfan Wicaksana <irfan.wicaksana@jojonomic.com>
2022-12-06 07:30:28 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
-module "s3-bucket" { 
-  source  = "app.terraform.io/Irfan/s3-bucket/aws"
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "my-s3-bucket"
   acl    = "private"
@@ -8,4 +8,8 @@ module "s3-bucket" {
     enabled = true
   }
 
+}
+module "s3-bucket" {
+  source  = "app.terraform.io/Irfan/s3-bucket/aws"
+  version = "2.8.0"
 }
